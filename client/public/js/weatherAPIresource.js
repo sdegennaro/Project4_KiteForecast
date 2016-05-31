01791f27bf8abb90e25c23e8231ac191effc676d
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-05-31 16:22:11 -0400

    adds working weather api request to server side

--- client/public/js/weatherAPIresource.js
+++ client/public/js/weatherAPIresource.js
@@ -1,3 +1,15 @@
 $(function() {
   console.log('weather loaded...');
 });
+
+// http://api.worldweatheronline.com/premium/v1/marine.ashx
+
+function getForecast(long, lat){
+  $.ajax({
+    url: "/weatherapi?long="+ long+"&lat="+lat,
+    type: "GET",
+    success: function(data){
+      console.log(data);
+    }
+  })
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,12 +6,34 @@ var express = require('express'),
     */
     path = require('path');
 
+var request = require('request');
+
 router.get('/', function(req, res, next) {
   res.sendFile( path.resolve('client/public/views/index.html') );
 });
 
-router.get('/key',function(req,res,next){
-  res.json({API_KEY: process.env.WEATHER_API_KEY})
+router.get('/weatherapi',function(req,res,next){
+  // res.json({API_KEY: process.env.WEATHER_API_KEY})
+
+  console.log('http://api.worldweatheronline.com/premium/v1/marine.ashx?format=json&q='+req.query.long+','+req.query.lat+'&key='+process.env.WEATHER_API_KEY);
+  // request
+  //   .get('http://api.worldweatheronline.com/premium/v1/marine.ashx?format=json&q='+req.query.long+','+req.query.lat+'&key='+process.env.WEATHER_API_KEY)
+  //   .on('error', function(err) {
+  //     console.log(err)
+  //   })
+  //   .on('response', function(response){
+  //     console.log(response);
+  //     res.json(response)
+  //   })
+
+    request('http://api.worldweatheronline.com/premium/v1/marine.ashx?format=json&q='+req.query.long+','+req.query.lat+'&key='+process.env.WEATHER_API_KEY, function(error,response,body){
+      if(error){
+        console.log(error);
+      } else{
+        console.log(body);
+        res.json(body);
+      }
+    })
 })
 
 module.exports = router;
